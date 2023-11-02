c857998a1ae42921e143222f1bc31c2df290915f
Vadim Smagin <magenvadim@gmail.com>
2023-11-02 22:00:17 +0200

    #3 Components code refactor

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -3,15 +3,16 @@ import Category from './Category/Category'
 import Colors from './Colors/Colors'
 import Price from './Price/Price'
 
-function Sidebar() {
+function Sidebar({handleChange}) {
+
   return (
     <section className="sidebar">
         <div className="logo-container">
             <h1>🛒</h1>
         </div>
-        <Category/>
-        <Price/>
-        <Colors/>
+        <Category handleChange={handleChange}/>
+        <Price handleChange={handleChange}/>
+        <Colors handleChange={handleChange}/>
     </section>
   )
 }
