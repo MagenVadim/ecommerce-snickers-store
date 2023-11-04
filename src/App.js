7baca476b905b9e7c1d41e187d28ee5661fe3636
Vadim Smagin <magenvadim@gmail.com>
2023-11-04 18:15:33 +0200

    #6 Card Component refactor

--- src/App.js
+++ src/App.js
@@ -62,9 +62,9 @@ function App() {
   return (
     <>
         <Sidebar handleChange={handleChange}/>
-        <Navigation/>
-        <Recommended/>
-        <Products/>        
+        <Navigation query={query}handleInputChange={handleInputChange}/>
+        <Recommended handleClick={handleClick}/>
+        <Products result={result}/>        
     </>
   )  
 }
